8b7e0c238645e85744f02e3a7ada23a9974cf80d
Isaac Winner <110716580+isaacwinner62@users.noreply.github.com>
2023-01-23 15:00:43 -0800

    adding jquery code

---
+++ ABgoodluck.js
@@ -0,0 +1,7 @@
+$(function(){
+      $("#galllery").click(function(){
+            $(".classhide").css("display","inline");
+            $(".clashide").css("display","none")
+      });
+      $("")
+});
