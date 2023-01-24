5b6aba00ca3914d5eb09ec05fa0de0393eccc23d
Isaac Winner <110716580+isaacwinner62@users.noreply.github.com>
2023-01-24 02:12:37 -0800

    gggg

--- ABgoodluck.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-      $("#galllery").click(function(){
-            $(".classhide").css("display","inline");
-            $(".clashide").css("display","none")
-      });
-      $("")
-});
